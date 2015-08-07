925b2308e064cc1fe2d9740fa24904f0481b1f50
Willow Inc <otavio@Willows-MacBook-Pro.local>
2015-08-07 09:53:32 -0700

    visual changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $('.tarot-cards li').each(function(index){
 	$(this).children('img').attr('src', 'img/'+index+'.jpg');
 
+  /* open tarot card */
   $(this).on('click', function() {
     $('body').css('overflow', 'hidden');
     $('li').removeClass('show');
     $(this).addClass('show');
   })
 
+  /* close tarot card */
   $('h2').on('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -14,10 +16,32 @@ $('.tarot-cards li').each(function(index){
     $('li').removeClass('show');
   })
 
+  /* handle tables td */
   $('td').on('click', function() {
     $('table').removeClass('selected');
     $('td p').removeClass('selected');
     $(this).children('p').toggleClass('selected');
     $(this).parents('table').addClass('selected');
   })
+
+  /* LInk Handler */
+  $('a').on('click', function(e) {
+    var effectType = $(this).attr('rel');
+    var effectName = $(this).attr('href');
+
+    e.preventDefault();
+    $('ul.'+ effectType +' .'+ effectName +'').addClass('selected');  
+  })
+
+  /* flip handler */
+  $('ul li button').on('click', function() {
+    $(this).parent().addClass('flip');
+    $(this).parent().siblings().removeClass('flip');
+  })
+
+  /* close effect */
+  $('ul li h3').on('click', function() {
+    $(this).parents('li').removeClass('selected');
+  })
 })
+
