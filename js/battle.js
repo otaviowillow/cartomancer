16c52a34f14a4931a39d532ca62c2c622be266de
Willow Inc <otavio@Willows-MacBook-Pro.local>
2015-08-23 18:45:33 -0700

    battle working

--- js/battle.js
+++ js/battle.js
@@ -1,9 +1,13 @@
+Vue.filter('rollDice', function (max, min) {
+  return Math.round(Math.random() * (max - min) + min);
+})
+
 var demo = new Vue({
   el: '#battle',
   data: {
     player: 
     {
-      name: 'Player 1',
+      name: "Player 1",
       health: 100,
       armor: 0.20,
       attack: {
@@ -11,12 +15,79 @@ var demo = new Vue({
         min: 1,
         max: 4
       }
-    }
-    ,
+    },
+    trumps:
+    {
+      name: "The Fool",
+      number: "0",
+      battleEffect: "Confusion",
+      legendary: true,
+      battleProc: true,
+      rare: {
+        procChance: 50,
+        yellow: {
+          goodDescription: "At the end of your Querent phase, look at target opponent hand and pick a card. Separate that card. At the start of target opponents turn, that card is used on owner of the card",
+          badDescription: "At the end of your Querent phase, target opponent looks at your hand and picks a card. Separate that card. At the start of target opponents turn, that card is used on owner of the card",
+          battle: {
+            type: "damage",
+            description: "Target gets confused at the start of turn. 60% more damage from confusion hits",
+            modifier: 75
+          }
+        },
+        blue: {
+          goodDescription: "At the end of your Querent phase, look at target opponent hand and pick a card. Discard that card",
+          badDescription: "At the end of your Querent phase, target opponent looks at your hand and picks a card. Discard that card",
+          battle: {
+            type: "cc",
+            description: "Target gets confused at the start of turn. 20% chance to stun on confusion hit",
+            modifier: 25
+          }
+        },
+        other: {
+          goodDescription: "At the end of your Querent phase, look at target opponent hand and pick a card. Separate that card. Owner of that card may choose to discard a card and buy the separated card. Discard that card before the start of next turn",
+          badDescription: "At the end of your Querent phase, target opponent looks at your hand and picks a card. Separate that card. Owner of that card may choose to discard a card and buy the separated card. Discard that card before the start of next turn",
+          battle: {
+            type: "heal",
+            description: "Target gets confused at the start of turn. 40% chance to heal opponent on confusion hit",
+            modifier: 50
+          }
+        },
+        common: {
+          procChance: 25,
+          yellow: {
+            goodDescription: "At the end of your Querent phase, pick a random card from target opponents hand. Separate that card. At the start of target opponents turn, that card is used on owner of the card",
+            badDescription: "At the end of your Querent phase, target opponent picks a random card from your hand. Separate that card. At the start of target opponents turn, that card is used on owner of the card",
+            battle: {
+              type: "damage",
+              description: "Target gets confused at the start of turn. 30% more damage from confusion hits",
+              modifier: 30
+            }
+          },
+          blue: {
+            goodDescription: "At the end of your Querent phase, pick a random card from target opponents hand. Separate that card. Discard that card",
+            badDescription: "At the end of your Querent phase, target opponent picks a random card from your hand. Discard that card",
+            battle: {
+              type: "cc",
+              description: "Target gets confused at the start of turn. 10% chance to stun on confusion hit",
+              modifier: 10
+            }
+          },
+          other: {
+            goodDescription: "At the end of your Querent phase, pick a random card from target opponents hand. Separate that card. Owner of that card may choose to discard a card and buy the separated card. Discard that card before the start of next turn",
+            badDescription: "At the end of your Querent phase, target opponent picks a random card from your hand. Separate that card. Owner of that card may choose to discard a card and buy the separated card. Discard that card before the start of next turn",
+            battle: {
+              type: "heal",
+              description: "Target gets confused at the start of turn. 20% chance to heal opponent on confusion hit",
+              modifier: 20
+            }
+          }
+        }
+      }
+    },
     monsters: [
       { 
-        name: 'Main Monster',
-        type: 'main',
+        name: "Main Monster",
+        type: "main",
         level: 2,
         health: 20,
         armor: 0.20,
@@ -27,8 +98,8 @@ var demo = new Vue({
         }
       },
       { 
-        name: 'Add 1',
-        type: 'add',
+        name: "Add 1",
+        type: "add",
         level: 2,
         health: 10,
         armor: 0.20,
@@ -39,8 +110,8 @@ var demo = new Vue({
         }
       },
       { 
-        name: 'Add 2',
-        type: 'add',
+        name: "Add 2",
+        type: "add",
         level: 2,
         health: 10,
         armor: 0.20,
@@ -51,8 +122,8 @@ var demo = new Vue({
         }
       },
       { 
-        name: 'Main Monster',
-        type: 'main',
+        name: "Main Monster",
+        type: "main",
         level: 3,
         health: 40,
         armor: 0.20,
@@ -63,8 +134,8 @@ var demo = new Vue({
         }
       },
       { 
-        name: 'Add 1',
-        type: 'add',
+        name: "Add 1",
+        type: "add",
         level: 3,
         health: 10,
         armor: 0.20,
@@ -75,8 +146,8 @@ var demo = new Vue({
         }
       },
       { 
-        name: 'Add 2',
-        type: 'add',
+        name: "Add 2",
+        type: "add",
         level: 3,
         health: 10,
         armor: 0.20,
@@ -92,35 +163,32 @@ var demo = new Vue({
     rollDice: function(max, min) {
       return Math.round(Math.random() * (max - min) + min);
     },
+    dealDamage: function(targetMonster, damage, target) {
+      if(targetMonster.health > 0) {
+        target.health -= damage;
+      } else {
+        targetMonster.health = 0
+      }
+    },
     playerHit: function(monster) {
       playerFullDamage = this.player.attack.base + this.rollDice(this.player.attack.max, this.player.attack.min);
-      playerDamage = Math.round(playerFullDamage - (playerFullDamage * monster.armor))
+      playerDamage = Math.round(playerFullDamage - (playerFullDamage * monster.armor));
 
-      if(monster.health > 0) {
-        monster.health -= playerDamage;
-        console.log(this.player.name, 'hitting monster', playerDamage)
-      } else {
-        monster.health = 0
-      }
+      this.dealDamage(monster, playerDamage, monster)
     },
     monsterHit: function(monster) {
       monsterFullDamage = monster.attack.base + this.rollDice(monster.attack.max, monster.attack.min);
       monsterDamage = Math.round(monsterFullDamage - (monsterFullDamage * this.player.armor))
 
-      if(monster.health > 0) {
-        this.player.health -= monsterDamage;
-        console.log(monster.name, 'hitting player', this.player.health)
-      } else {
-        monster.health = 0;
-      }
+      this.dealDamage(monster, monsterDamage, this.player)
     },
-    battle: function(monster, lvl) {
+    battle: function(monster, selectedLevel) {
       self = this;
       self.playerHit(monster);
 
-      $(self.monsters).each(function(i, v) {
-        if(v.level == lvl) {
-          self.monsterHit(v)
+      $(self.monsters).each(function(i, monster) {
+        if(monster.level == selectedLevel) {
+          self.monsterHit(monster)
         } 
       })
     }
